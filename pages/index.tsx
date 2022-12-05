5855e80163e6aa1e47b6d5934f75f56377bca195
s13146-pj <56793702+s13146@users.noreply.github.com>
2022-12-05 13:38:47 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -84,22 +84,22 @@ export default function Home() {
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'>
-              <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+              <Image src={web1} className="rounded-lg object-cover" layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-            <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            <Image src={web1} className="rounded-lg object-cover"  layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-            <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            <Image src={web1} className="rounded-lg object-cover"  layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-            <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            <Image src={web1} className="rounded-lg object-cover"  layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-            <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            <Image src={web1} className="rounded-lg object-cover"  layout="responsive"/>
             </div>
             <div className='basis-1/3 flex-1'>
-            <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'}layout="responsive"/>
+            <Image src={web1} className="rounded-lg object-cover" layout="responsive"/>
             </div>
           </div>
         </section>
